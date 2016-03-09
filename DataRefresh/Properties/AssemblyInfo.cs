ee2041a64d177c18738c184567d5f304f880ecf3
Anthony Perez <anthony@amazingant.com>
2016-03-09 18:20:01 +0000

    Update version to 0.2.0, and adjust copyright year info

--- DataRefresh/Properties/AssemblyInfo.cs
+++ DataRefresh/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DataRefresh")]
 [assembly: AssemblyDescription("Collection of interfaces and base implementations for different ways data may need to be loaded/refreshed")]
 [assembly: AssemblyProduct("DataRefresh")]
-[assembly: AssemblyCopyright("Copyright © Anthony Perez 2014")]
+[assembly: AssemblyCopyright("Copyright © Anthony Perez 2014-2016")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0.0")]
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ DataRefresh
 Collection of interfaces and base implementations for different data
 models
 
-This library is Copyright (C) 2014 Anthony Perez (@amazingant), and is
+This library is Copyright (C) 2014-2016 Anthony Perez (@amazingant), and is
 licensed under version 2.1 of the GNU LGPL license as provided in the
 LICENSE file.
